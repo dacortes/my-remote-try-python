34e9b06f8785945003bf0c167858bc7773c68ebe
dacortes <87203430+dacortes@users.noreply.github.com>
2023-11-14 10:07:59 +0000

    [IMP] swap variable in html with python

--- app.py
+++ app.py
@@ -3,9 +3,23 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
+from flask import Flask, send_file
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return app.send_static_file("index.html")
+    print("init app")
+    message = "¡Hola judador !"
+    static_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
+    index_path = os.path.join(static_dir, 'index.html')
+
+    with open(index_path, 'r') as file:
+        content = file.read()
+        modified_content = content.replace('{{ message }}', message)
+
+    with open('index.html', 'w') as modified_file:
+        modified_file.write(modified_content)
+
+    return send_file('index.html')
